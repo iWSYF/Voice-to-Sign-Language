7a756d41a33e13761cce1c7569b8df1e608fadfa
Wail <wsyf.96@hotmail.com>
2025-03-29 16:01:50 +0300

    Fix the images and put it ready to show

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from vosk import Model, KaldiRecognizer 
 import pyaudio
 import json 
+import os
 
 app = Flask(__name__) 
 
@@ -15,7 +16,10 @@ p = None
 stop_this_words = [
     "any", "much", "many", "a", "an", "the", "am", "is", "are", "was", "were", "be", "been", "being",
     "have", "has", "had", "do", "does", "did", "he", "she", "it", "they",
-    "and", "but", "or", "nor", "yet", "so", "i'm", 
+    "and", "but", "or", "nor", "yet", "so", "i'm", "you're", "he's", "she's", "it's", "we're", "they're",
+    "i've", "you've", "we've", "they've","i'll", "you'll", "he'll", "she'll", "we'll", "they'll",
+    "i'd", "you'd", "he'd", "she'd", "we'd", "they'd","isn't", "aren't", "wasn't", "weren't",
+    "hasn't", "haven't", "hadn't","might've", "must've", "could've", "should've", "would've" 
 ]
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,6 +27,7 @@ def home():
     global recognizer, stream  
     text = ""  
     words = []  
+    images_to_show = []
 
     if request.method == 'POST':
         for _ in range(0, int(44100 / 1024 * 5)):  # Recording for 5 seconds
@@ -36,10 +41,23 @@ def home():
 
         words = text.strip().split()  
         filtered_words = [word for word in words if word.lower() not in stop_this_words]
-        print("Filtered words:", filtered_words)  
-        return render_template('index.html', original_text=text.strip(), words=filtered_words)
 
-    return render_template('index.html', original_text=None, words=None)
+        # 🔎 نبحث داخل مجلد ASL عن صور تطابق الكلمات
+        asl_path = os.path.join(app.static_folder, "ASL")
+        asl_images = os.listdir(asl_path)
+
+        for word in filtered_words:
+            for img in asl_images:
+                img_name = os.path.splitext(img)[0].lower()
+                if word.lower() == img_name:
+                    images_to_show.append(f"ASL/{img}")
+
+        print("Filtered words:", filtered_words)  
+        print("Images to show:", images_to_show)
+
+        return render_template('index.html', original_text=text.strip(), words=filtered_words, images=images_to_show)
+
+    return render_template('index.html', original_text=None, words=None, images=None)
 
 # for about.html page
 @app.route('/about')
@@ -53,7 +71,7 @@ if __name__ == '__main__':
       # C:\Users\wsyf9\OneDrive\Desktop\vosk-model-en-us-0.42-gigaspeech - Wail
       # C:\Users\wsyf9\OneDrive\Desktop\vosk-model-en-us-0.22
       
-    recognizer = KaldiRecognizer(model, 44100)  # Initialize the recognizer with the model and sample rate (44100 Hz)
+    recognizer = KaldiRecognizer(model, 44100)  
 
     p = pyaudio.PyAudio()  
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)  
